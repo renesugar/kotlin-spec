73be37fa3d130d82039be82c18c71cc811960348
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2019-08-28 16:01:16 +0300

    Inline diagramming for everybody! And some actual diagrams

--- docs/scripts/build/buildPdfBySections.sh
+++ docs/scripts/build/buildPdfBySections.sh
@@ -11,7 +11,7 @@ gpp -H ./~temp.md | pandoc \
 --filter ${FILTERS_DIR}/markSentencesFilter.sh \
 --filter ${FILTERS_DIR}/copyPasteFilter.sh \
 --filter ${FILTERS_DIR}/inlineDiagrams.sh \
---filter ${FILTERS_DIR}/inlineCodeIndenter.sh \
+--filter ${FILTERS_DIR}/mathCleanUpFilter.sh \
 -H ./preamble.tex --variable documentclass=book \
 -s -f markdown-raw_html+smart+tex_math_double_backslash \
 -o ${BUILD_DIR}/pdf/sections/<section>.pdf
--- scripts/build/buildHtml.sh
+++ scripts/build/buildHtml.sh
@@ -9,7 +9,7 @@ gpp -H ./index.md | pandoc \
 --filter ${FILTERS_DIR}/markSentencesFilter.sh \
 --filter ${FILTERS_DIR}/copyPasteFilter.sh \
 --filter ${FILTERS_DIR}/inlineDiagrams.sh \
---filter ${FILTERS_DIR}/inlineCodeIndenter.sh \
+--filter ${FILTERS_DIR}/mathCleanUpFilter.sh \
 --toc --toc-depth=6 -c ./${RESOURCES_DIR}/css/main.css \
 --katex=${BUILD_DIR}/html/resources/js/katex/ \
 -H ./preamble.md -s -f markdown-raw_html+smart+tex_math_double_backslash \
